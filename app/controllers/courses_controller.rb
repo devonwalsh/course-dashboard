acdd1900066dfb8a0caff4a3e62f01b32d755119
Devon Walsh <devonwalsh@gmail.com>
2021-08-04 10:34:44 -0400

    Refactoring and debugging

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,12 +15,12 @@ class CoursesController < ApplicationController
         render json: course, status: :created
     end
 
-    def save
-        user = User.find_by(id: session[:user_id])
-        course = Course.find_by(id: course_params[:id])
-        user.courses << course
-        render json: course, status: :created
-    end
+    # def save
+    #     user = User.find_by(id: session[:user_id])
+    #     course = Course.find_by(id: course_params[:id])
+    #     user.courses << course
+    #     render json: course, status: :created
+    # end
 
     def unsave
         user = User.find_by(id: session[:user_id])
